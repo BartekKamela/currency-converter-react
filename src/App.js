00fbd39f7fea5371bc6340fafd2b9527300f18e8
Bartlomiej Kamela <bartek@lubnet.pl>
2023-04-07 20:26:32 +0200

    state hook added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import Form from "./Form";
 import Footer from "./Footer";
+import { useState } from 'react';
+import { currencies } from './currencies';
 
 function App() {
+  const [result, setResult] = useState("N/A");
+
   return (
     <div>
         <main>
-            <Form />
+            <Form result={result} />
         </main>
         <Footer />
     </div>
   );
-}
+};
 
 export default App;
