fe234e3637bde2d9e41c309d7245929634f6cefb
Bartlomiej Kamela <bartek@lubnet.pl>
2023-04-07 18:42:44 +0200

    currencies object added

---
+++ src/currencies.js
@@ -0,0 +1,7 @@
+export const currencies = [
+    { id: 1, name: "PLN", value: 1},
+    { id: 2, name: "EUR", value: 4.7186 },
+    { id: 3, name: "USD", value: 4.3242 },
+    { id: 4, name: "GBP", value: 5.3648 },
+    { id: 5, name: "CHF", value: 4.7118 },
+  ];
