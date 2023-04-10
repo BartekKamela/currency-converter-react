dcebffbcbfcabc8025cc132e83c2a4e81e2564d0
Bartlomiej Kamela <bartek@lubnet.pl>
2023-04-10 10:44:00 +0200

    changing currencies object

--- src/App/currencies.js
+++ src/App/currencies.js
@@ -1,7 +1,7 @@
 export const currencies = [
-    { id: 1, name: "PLN", value: 1},
-    { id: 2, name: "EUR", value: 4.7186 },
-    { id: 3, name: "USD", value: 4.3242 },
-    { id: 4, name: "GBP", value: 5.3648 },
-    { id: 5, name: "CHF", value: 4.7118 },
+    { name: "PLN", value: 1},
+    { name: "EUR", value: 4.7186 },
+    { name: "USD", value: 4.3242 },
+    { name: "GBP", value: 5.3648 },
+    { name: "CHF", value: 4.7118 },
   ];
