2956e5531c4c1981f08a55eb6b7eb95a3ae6a55d
Bartlomiej Kamela <bartek@lubnet.pl>
2023-04-15 19:50:36 +0200

    the Effect Hook added

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -1,8 +1,19 @@
+import { useState, useEffect } from 'react';
 import "./style.css";
 
 const Clock = () => {
+    const [date, setDate] = useState(new Date());
 
-    const date = new Date();
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
 
     return (
         <header className="clock">
