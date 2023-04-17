af67cfbca4b1498c8144f9170c7f00a3ab434f5f
Bartlomiej Kamela <bartek@lubnet.pl>
2023-04-17 12:21:00 +0200

    declaring a constant with a formatted date

--- src/App/Form/Clock/index.js
+++ src/App/Form/Clock/index.js
@@ -14,20 +14,18 @@ const Clock = () => {
         };
     }, []);
 
+    const formattedDate = date.toLocaleString(undefined, {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+    })
 
     return (
         <div className="clock">
-            Dzisiaj jest&nbsp;
-            {date.toLocaleString(
-                "pl-PL",
-                {
-                    month: "long",
-                    weekday: "long",
-                    day: "numeric",
-                },
-            )}
-            {", "}
-            {date.toLocaleTimeString("pl-PL")}
+            Dzisiaj jest {formattedDate}
         </div>
     )
 };
