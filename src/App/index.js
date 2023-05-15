3e309368161169a653b127f186c06c60b680c1ab
Bartlomiej Kamela <bartek@lubnet.pl>
2023-05-15 20:07:13 +0200

    removed state Hook and calculatedResult from App

--- src/App/index.js
+++ src/App/index.js
@@ -1,34 +1,15 @@
 import Form from "./Form";
 import Footer from "./Footer";
-import { useState } from 'react';
-import { currencies } from './currencies';
 
 function App() {
-  const [result, setResult] = useState();
-
-  const calculateResult = (amount, currencyIn, currencyOut) => {
-    const valueIn = currencies.find(({ name }) => name === currencyIn).value;
-    const valueOut = currencies.find(({ name }) => name === currencyOut).value; 
-    const rate = valueIn / valueOut;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount * rate,
-      sourceCurrency: currencyIn,
-      targetCurrency: currencyOut
-    });
-  };
-
+  
   return (
-      <div>
-        <main>
-            <Form 
-              result={result} 
-              calculateResult={calculateResult} 
-            />
-        </main>
-        <Footer />
-      </div>
+    <div>
+      <main>
+        <Form />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
