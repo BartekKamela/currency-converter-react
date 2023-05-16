d8322451ee54ecff882e78173a67c4eeb13842a9
Bartlomiej Kamela <bartek@lubnet.pl>
2023-05-16 19:24:01 +0200

    added breakpoints

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -3,7 +3,7 @@ import Clock from "./Clock";
 import {
     StyledForm, FormFieldset, FormLegend, Amount, Label, Input,
     Fieldset, Legend, CurrencyIn, CurrencyOut, Select,
-    Wrapper, Button, ButtonCount
+    Wrapper, Button, ButtonCount, Loading, Fail
 } from "./styled";
 import { useState } from "react";
 import { currencies } from '../currencies';
@@ -14,29 +14,29 @@ const Form = () => {
     const [currencyIn, setCurrencyIn] = useState("PLN");
     const [currencyOut, setCurrencyOut] = useState("EUR");
     const [result, setResult] = useState();
-    const ratesData = useRatesData(currencyIn);
+    const ratesData = useRatesData();
 
     const changeCurrency = () => {
         setCurrencyIn(currencyOut);
         setCurrencyOut(currencyIn);
     };
-  
+
     const calculateResult = (amount, currencyIn, currencyOut) => {
-      const sourceRate = ratesData.rates[currencyIn];
-      const targetRate = ratesData.rates[currencyOut];
-     
-      setResult({
-        sourceAmount: +amount,
-        targetAmount: (targetRate / sourceRate) * amount,
-        sourceCurrency: currencyIn,
-        targetCurrency: currencyOut
-      });
-  
+        const sourceRate = ratesData.rates[currencyIn];
+        const targetRate = ratesData.rates[currencyOut];
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: (targetRate / sourceRate) * amount,
+            sourceCurrency: currencyIn,
+            targetCurrency: currencyOut
+        });
+
     };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        calculateResult(amount, currencyIn, currencyOut);
+        calculateResult(amount, currencyIn, currencyOut, ratesData);
     };
 
     return (
@@ -44,68 +44,85 @@ const Form = () => {
             <FormFieldset>
                 <FormLegend>KALKULATOR WALUT</FormLegend>
                 <Clock />
-                <Amount>
-                    <Label>
-                        Podaj kwotę*: &nbsp;
-                        <Input
-                            value={amount}
-                            type="number"
-                            min="0"
-                            max="9999999999"
-                            step="0.01"
-                            placeholder="Wpisz kwotę"
-                            required
-                            onChange={({ target }) => setAmount(target.value)}
-                        />
-                    </Label>
-                </Amount>
-                <Fieldset>
-                    <Legend>Wybierz waluty</Legend>
-                    <p>
-                        <CurrencyIn>
-                            Mam:
-                            <Select
-                                value={currencyIn}
-                                onChange={({ target }) => setCurrencyIn(target.value)}
-                            >
-                                {currencies.map((currency) => (
-                                    <option
-                                        key={currency.id}
-                                        value={currency.name}
-                                    >
-                                        {currency.name}
-                                    </option>
-                                ))}
-                            </Select>
-                        </CurrencyIn>
-                        <Button type="button" onClick={changeCurrency}>
-                            {"<=>"}
-                        </Button>
-                        <CurrencyOut>
-                            Chcę:
-                            <Select
-                                value={currencyOut}
-                                onChange={({ target }) => setCurrencyOut(target.value)}
-                            >
-                                {currencies.map((currency) => (
-                                    <option
-                                        key={currency.id}
-                                        value={currency.name}
-                                    >
-                                        {currency.name}
-                                    </option>
-                                ))}
-                            </Select>
-                        </CurrencyOut>
-                    </p>
-                </Fieldset>
-                <ButtonCount>Przelicz</ButtonCount>
-                <Result result={result} />
-                <Wrapper>
-                    Kurs walut z {ratesData.date} r. pobrane z Europejskiego Banku Centralnego.
-                </Wrapper>
+                {ratesData.status === "loading"
+                    ? (
+                        <>
+                            <Loading>
+                                Pobieranie danych. Proszę czekać.
+                            </Loading>
+                        </>
+                    ) : ratesData.status === "error"
+                        ? (
+                            <Fail>
+                                Coś poszło nie tak. Sprawdź swoje połączenie z
+                                internetem lub spróbuj ponownie później.
+                            </Fail>
+                        ) : (
+                            <>
+                                <Amount>
+                                    <Label>
+                                        Podaj kwotę*: &nbsp;
+                                        <Input
+                                            value={amount}
+                                            type="number"
+                                            min="0"
+                                            max="9999999999"
+                                            step="0.01"
+                                            placeholder="Wpisz kwotę"
+                                            required
+                                            onChange={({ target }) => setAmount(target.value)}
+                                        />
+                                    </Label>
+                                </Amount>
+                                <Fieldset>
+                                    <Legend>Wybierz waluty</Legend>
+                                    <p>
+                                        <CurrencyIn>
+                                            Mam:
+                                            <Select
+                                                value={currencyIn}
+                                                onChange={({ target }) => setCurrencyIn(target.value)}
+                                            >
+                                                {currencies.map((currency) => (
+                                                    <option
+                                                        key={currency.id}
+                                                        value={currency.name}
+                                                    >
+                                                        {currency.name}
+                                                    </option>
+                                                ))}
+                                            </Select>
+                                        </CurrencyIn>
+                                        <Button type="button" onClick={changeCurrency}>
+                                            {"<=>"}
+                                        </Button>
+                                        <CurrencyOut>
+                                            Chcę:
+                                            <Select
+                                                value={currencyOut}
+                                                onChange={({ target }) => setCurrencyOut(target.value)}
+                                            >
+                                                {currencies.map((currency) => (
+                                                    <option
+                                                        key={currency.id}
+                                                        value={currency.name}
+                                                    >
+                                                        {currency.name}
+                                                    </option>
+                                                ))}
+                                            </Select>
+                                        </CurrencyOut>
+                                    </p>
+                                </Fieldset>
+                                <ButtonCount>Przelicz</ButtonCount>
+                                <Result result={result} />
+                                <Wrapper>
+                                    Kurs walut pobrany z Europejskiego Banku Centralnego z {ratesData.date}.
+                                </Wrapper>
+                            </>
+                        )}
             </FormFieldset>
-        </StyledForm>
+        </StyledForm >
     )
 };
 
